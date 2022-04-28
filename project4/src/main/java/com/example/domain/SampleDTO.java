119c41fd699e8aa3d1a166a8eb79252993cffa2c
soapway <99641600+soapway@users.noreply.github.com>
2022-04-28 14:22:33 +0900

    chap6 test 1st

---
+++ project4/src/main/java/com/example/domain/SampleDTO.java
@@ -0,0 +1,9 @@
+package com.example.domain;
+
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+	private String name;
+	private int age;
+}
---
+++ project4/src/main/java/com/example/domain/SampleDTOList.java
@@ -0,0 +1,14 @@
+package com.example.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleDTOList {
+	private List<SampleDTO> list;
+
+	public SampleDTOList() {
+		list = new ArrayList<SampleDTO>();
+	}
+	
+	
+}
